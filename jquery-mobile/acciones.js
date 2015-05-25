0efb0352a476949b7ec9af909106e928315cbf7c
jairmn23 <damcb164e23@gmail.com>
2015-05-25 14:03:11 -0500

    5asubida
    khlkjh

--- jquery-mobile/acciones.js
+++ jquery-mobile/acciones.js
@@ -4,25 +4,28 @@ $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		
 		$('#izquierda').on ("swipeleft",function(){
-			navigator.notification.alert("Deslizó a la izquierda",function(){"Apliacion5","Aceptar"});
+			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion5","Aceptar"});
 		});//barrer izquierda
 		
-		$('#derecha').on ("swiperight",function(){
-			navigator.notification.confirm("¿Que quieres hacer?",function(apt){
-		switch(opt)
-		{
 		
-		case 1:
-		navigator.notification.beep(1);
-		break;
-		
-		case 2:
-		navigator.notification.vibrate(1000);
-		break;
-		}//switch opt
-			},"Aplicacion5","beep,Vibrar,Cancelar");//confirm
-		
+		$('#derecha').on ("swiperight", function() {
+			navigator.notification.confirm("�Que quieres hacer?",function(opt){
+				switch(opt)
+				{
+					
+					case 1:
+					navigator.notification.beep(1);
+					break;
+					
+					case 2:
+					navigator.notification.vibrate(1000);
+					break;
+				}//switch opt
+			},"Aplicacion5","Beep,Vibrar,Cancelar");//confirm
+			
 		});//barrer derecha
-	},false);//ready device
-	})//document
 		
+		
+	},false);//ready device
+});//document
+
